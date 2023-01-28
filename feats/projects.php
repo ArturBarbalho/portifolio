7dff8dedffe3131d4de1301c65781377119ea36d
Artur vinicius umbelino barbalho <arthurvinicius907@gmail.com>
2023-01-28 07:53:50 -0300

    .

---
+++ feats/projects.php
@@ -0,0 +1,37 @@
+<?php
+$projects = [
+    [
+        "link"=>"https://artmovies.netlify.app/",
+        "code"=>"https://github.com/ArturBarbalho/MoviesRemaster",
+        "image"=>"public/movies.png",
+        "name"=>"Movies and shows",
+        "description"=>"Site de filmes e series que consome api pública, feito em Next js utilizando Test Driven Development (TDD)"
+    ],
+    [
+        "link"=>"https://artlan.netlify.app/",
+        "code"=>"https://github.com/ArturBarbalho/landin_page_portifolio",
+        "image"=>"public/landingnew.png",
+        "name"=>"Landing comercial ",
+        "description"=>"Projeto com foco em design e responsividade, feito com boas
+        práticas.
+        "
+    ],
+    [
+        "link"=>"https://artcrud.netlify.app/",
+        "code"=>"https://github.com/ArturBarbalho/crud",
+        "image"=>"public/crud.png",
+        "name"=>"Crud",
+        "description"=>"Site com sistema de login que permite ver, cadastrar e deletar
+        produtos. projeto feito inteiro em Next js utilizando Prisma e
+        Postgree"
+    ],
+    [
+        "link"=>"",
+        "code"=>"https://github.com/ArturBarbalho/LandingPage",
+        "image"=>"public/n.png",
+        "name"=>"Landing test",
+        "description"=>""
+    ],
+    
+];
+?>
